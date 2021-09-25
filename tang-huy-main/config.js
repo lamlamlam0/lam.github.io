5fe399f0cb386786372265ac240b8fad079783bd
lamlamlam0 <87164199+lamlamlam0@users.noreply.github.com>
2021-09-25 22:08:35 +0700

    Add files via upload

---
+++ tang-huy-main/config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Lợn à!',
+    introDesc: `Tao biết là mày rất thích quà  
+    Nên t tặng cho mày một món quà nhỏ
+    Mong mày thich:))
+    lol`,
+    btnIntro: 'Uh t thích lắm hihi',
+    title: 'Mình có 1 câu hỏi muốn hỏi bạn:)), bạn muốn trả lời không:P',
+    desc: ' ',
+    btnYes: 'Có :333',
+    btnNo: 'Không bạn à :33',
+    question:'Trên thế giới hơn 7 tỉ người mà sao mày lại đòi quà t 😊',
+    btnReply: 'Thank you 🥰🥰🥰',
+    reply: 'Vì bạn là người đẹp trai nhất trong 7 tỉ người đó (¬‿¬)',
+    mess: 'Mình biết mà 🥰. chúc bạn có sinh nhật vui vẻ 🥰🥰',
+    messDesc: 'Từ lần sau không được đòi quà nữa nhé :)).',
+    btnAccept: 'Okiiiii lun 😎',
+    messLink: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+}
